35c42b33fe31adba9c0911e0cda304bd5c7f7421
Sergiu <53493788+sergebyte@users.noreply.github.com>
2024-11-14 10:15:41 +0200

    Convert Display.java

--- working-with-classes/src/main/java/org/sergedb/oop/classes/display/Display.java
+++ working-with-classes/src/main/java/org/sergedb/oop/classes/display/Display.java
@@ -1,32 +1,5 @@
 package org.sergedb.oop.classes.display;
 
-public class Display {
-    private final int width;
-    private final int height;
-    private final float ppi;
-    private final String model;
-
-    public Display(int width, int height, float ppi, String model) {
-        this.width = width;
-        this.height = height;
-        this.ppi = ppi;
-        this.model = model;
-    }
-
-    public int getWidth() {
-        return width;
-    }
-
-    public int getHeight() {
-        return height;
-    }
-
-    public float getPpi() {
-        return ppi;
-    }
-
-    public String getModel() {
-        return model;
-    }
+public record Display(int width, int height, float ppi, String model) {
 
 }
